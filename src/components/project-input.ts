83cd73a18c402b90fc9200170f718c3ecff9c645
Marat-Akhalov <117514563+Marat-Akhalov@users.noreply.github.com>
2024-09-05 17:03:18 +0400

    feat: add validation logic and decorators to handle this context

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,3 +1,6 @@
+import { AutoBind } from '../decorators/autobind';
+import { projectState } from '../state/project-state';
+import { Validatable, validate } from '../utils/validation';
 import { Component } from './base-component';
 
 export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
@@ -19,9 +22,67 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     this.peopleInputElement = document.querySelector(
       '#people'
     ) as HTMLInputElement;
+
+    this.configure();
   }
 
-  configure(): void {}
+  configure(): void {
+    this.element.addEventListener('submit', this.submitHandler);
+  }
 
   renderContent(): void {}
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true,
+    };
+
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    };
+
+    const peopleValidatable: Validatable = {
+      value: enteredPeople,
+      required: true,
+      min: 1,
+      max: 5,
+    };
+
+    if (
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
+    ) {
+      alert('Invalid input, please try again');
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, parseFloat(enteredPeople)];
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.peopleInputElement.value = '';
+  }
+
+  @AutoBind
+  private submitHandler(e: Event) {
+    e.preventDefault();
+
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput;
+      projectState.addProject(title, description, people);
+    }
+
+    this.clearInputs();
+  }
 }
